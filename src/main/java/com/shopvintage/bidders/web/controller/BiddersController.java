dc34b21f8c53db32d1b1fec3291b29be9d2f9f94
soabel <alfredo.benaute@gmail.com>
2021-02-19 19:50:34 -0500

    add methods

--- src/main/java/com/shopvintage/bidders/web/controller/BiddersController.java
+++ src/main/java/com/shopvintage/bidders/web/controller/BiddersController.java
@@ -3,6 +3,7 @@ package com.shopvintage.bidders.web.controller;
 import com.shopvintage.bidders.persistence.entities.Bidder;
 import com.shopvintage.bidders.persistence.repositories.BiddersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -23,9 +24,31 @@ public class BiddersController {
         return this.biddersRepository.findAll();
     }
 
+    @GetMapping("/{id}/{name}")
+    public Bidder findById(@PathVariable("id") Integer id, @PathVariable("name") String name){
+        System.out.println("name = " + name);
+        return this.biddersRepository.findById(id).get();
+    }
+
+    @GetMapping("find")
+    public Bidder find(@Param("id") Integer id, @Param("name") String name){
+        System.out.println("name = " + name);
+        return this.biddersRepository.findById(id).get();
+    }
+
     @PostMapping
     public Bidder save(@RequestBody Bidder bidder){
         return this.biddersRepository.save(bidder);
+    }
+
+    @PatchMapping
+    public Bidder update(@RequestBody Bidder bidder){
+        return this.biddersRepository.save(bidder);
+    }
+
+    @DeleteMapping("/{id}")
+    public void delete(@PathVariable("id") Integer id){
+         this.biddersRepository.deleteById(id);
     }
 
 }
